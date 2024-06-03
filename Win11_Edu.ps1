f63eab52985bb355fcc46be7e6a218cb4a162fef
Michael Pasqualone <michaelp85@users.noreply.github.com>
2024-06-04 09:39:39 +1000

    Create Win11_Edu.ps1
    Version 0.1

---
+++ Win11_Edu.ps1
@@ -0,0 +1,31 @@
+# Settings
+$OSName = 'Windows 11 23H2 x64'
+$OSEdition = 'Education'
+$OSActivation = 'Retail'
+$OSLanguage = 'en-us'
+
+#Set Global OSDCloud Vars
+$Global:MyOSDCloud = [ordered]@{
+    Restart = [bool]$False
+    RecoveryPartition = [bool]$true
+    OEMActivation = [bool]$True
+    WindowsUpdate = [bool]$true
+    WindowsUpdateDrivers = [bool]$true
+    WindowsDefenderUpdate = [bool]$true
+    SetTimeZone = [bool]$true
+    ClearDiskConfirm = [bool]$False
+    ShutdownSetupComplete = [bool]$false
+    SyncMSUpCatDriverUSB = [bool]$true
+    CheckSHA1 = [bool]$true
+}
+
+#Launch OSDCloud
+Write-Host "Starting OSDCloud" -ForegroundColor Green
+
+Write-Host -ForegroundColor Green "Updating OSD Powershell Module"
+Install-Module OSD -Force
+Write-Host -ForegroundColor Green "Importing OSD Powershell Module"
+Import-Module OSD -Force
+
+#write-host "Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage"
+#Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
