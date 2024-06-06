66830d9647a2df21dadaaaf8a6cc0bb5350d8de1
Michael Pasqualone <michaelp85@users.noreply.github.com>
2024-06-06 11:55:42 +1000

    Update Win11_Edu.ps1
    V3

--- Win11_Edu.ps1
+++ Win11_Edu.ps1
@@ -20,8 +20,12 @@ $Global:MyOSDCloud = [ordered]@{
     SyncMSUpCatDriverUSB = [bool]$True
     CheckSHA1 = [bool]$True
 }
-Write-Host "Version 0.2 by MP"
+Write-Host "Version 0.3 by MP"
 Start-Sleep 5
 
+Write-Host "Autopilot registering this device and uploading hash"
+start /wait PowerShell -NoL -C Invoke-WebPSScript 'https://raw.githubusercontent.com/AIEIT/OSDCloud/main/Autopilot.ps1'
+
 Write-Host "Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage"
+Start-Sleep 5
 Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
